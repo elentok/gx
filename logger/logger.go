4bf0591df60087c16e18c162bdce8a9d12b9501d
David Elentok <3david@gmail.com>
2026-08-07 19:19:04 +0300

    logger: write debug log to .scratch/gx.log

--- logger/logger.go
+++ logger/logger.go
@@ -3,16 +3,22 @@ package logger
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	tea "charm.land/bubbletea/v2"
 )
 
 const (
-	LOG_FILE = "gx.log"
+	LOG_FILE = ".scratch/gx.log"
 )
 
 func Debug(format string, args ...any) {
 	// if len(os.Getenv("DEBUG")) > 0 {
+	if err := os.MkdirAll(filepath.Dir(LOG_FILE), 0755); err != nil {
+		fmt.Println("fatal (can't create log dir):", err)
+		os.Exit(1)
+	}
+
 	f, err := tea.LogToFile(LOG_FILE, "debug")
 	if err != nil {
 		fmt.Println("fatal (can't open log file):", err)
